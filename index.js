87f26a701651e733847bef2199a21017322274ba
Theresa <tpgarcia293@gmail.com>
2021-04-04 15:23:25 -0400

    Update index.js
    icon element

--- index.js
+++ index.js
@@ -28,6 +28,10 @@ function showWeather(response) {
   document.querySelector(".temperature").innerHTML = Math.round(
     response.data.main.temp
   );
+  let iconElement = document.querySelector(#icon);
+  iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description); 
 
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
   document.querySelector("#wind").innerHTML = Math.round(
